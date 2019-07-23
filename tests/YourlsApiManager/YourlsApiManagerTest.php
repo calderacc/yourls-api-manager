f9845019b7fb335c69a1ed3c02b141c116f734a3
Malte Hübner <maltehuebner@gmx.org>
2019-07-23 08:38:23 +0200

    Add testUpdateShorturl.

--- tests/YourlsApiManager/YourlsApiManagerTest.php
+++ tests/YourlsApiManager/YourlsApiManagerTest.php
@@ -71,4 +71,36 @@ class YourlsApiManagerTest extends TestCase
 
         $this->assertTrue($response);
     }
+
+    public function testUpdateShorturl(): void
+    {
+        $curlResponse = new \stdClass();
+        $curlResponse->shorturl = 'foobarbaz';
+        $curlResponse->statusCode = 200;
+
+        $expectedRequestData = [
+            'username' => 'testusername',
+            'password' => 'testpassword',
+            'action' => 'update',
+            'format' => 'json',
+            'shorturl' => 'foobarbaz',
+            'url' => 'https://criticalmass.one/'
+        ];
+
+        $curl = $this->createMock(Curl::class);
+        $curl
+            ->expects($this->once())
+            ->method('post')
+            ->with($this->equalTo('testurl'), $this->equalTo($expectedRequestData))
+            ->will($this->returnValue($curlResponse));
+
+        $http = new Http('testurl', 'testusername', 'testpassword');
+        $http->setCurl($curl);
+
+        $yourlsApiManager = new YourlsApiManager($http);
+
+        $response = $yourlsApiManager->updateShorturl('foobarbaz', 'https://criticalmass.one/');
+
+        $this->assertTrue($response);
+    }
 }
